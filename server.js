ea4edccbad35a52bf691653400791debc9879282
LonelyNate <44495176+LonelyNate@users.noreply.github.com>
2019-01-16 17:07:28 +0000

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.listen(port, function() {
 
 app.use(express.static('Public'));
 
+
+app.get('/', function(req, res) {
+
+    
+    res.render('index');
+});
+
 console.log("my socket server is running");
 
 var socket = require('socket.io');
