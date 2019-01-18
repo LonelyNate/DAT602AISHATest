3aa754168b0a2c0d1ef1e1ca81b43136136a2049
LonelyNate <44495176+LonelyNate@users.noreply.github.com>
2019-01-18 00:38:14 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 
 var express = require('express');
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
+
 
 var app = express();
 
 app.use(express.static('Public'));
 
 console.log("my socket server is running");
+
+var socket = require ('socket.io');
