292da77519facb28e543d9518656de943b0d4acd
LonelyNate <44495176+LonelyNate@users.noreply.github.com>
2019-01-23 07:49:07 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,55 @@
 
+// This function handles an incoming "request"
+// And sends back out a "response";
+var handleRequest = function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World\n');
+};
+
+// HTTP module
+var http = require('http');
+
+// Create a server with the handleRequest callback
+var server = http.createServer(handleRequest);
+// Listen on port 8080
+server.listen(8080);
+
+console.log('Server started on port 8080');
+
+
+
+
+
+
+
+/*
 var express = require('express');
 
-var server = process.env.PORT || 5000;
+
 
 var app = express();
 
 app.use(express.static('Public'));
 
+app.get('/', function(req, res) {	
+
+
+     res.render('index.html');	
+});
+
 console.log("my socket server is running");
 
 var socket = require ('socket.io');
-
-var io = socket(server);
 
 io.sockets.on('connection' , newConnection);
 
 function newConnection(socket)
 {
       console.log('new connection  ' + socket.id);
+     socket.on('people' , mouseMsg);
+     function mouseMsg (data)
+     {
+          console.log(data)
+     }
 }
+*/
